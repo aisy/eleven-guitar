bcc9d0b6e283bcdfd7a0b69456bfe63af786db0a
Aisy Rozsidhy <aisymuhammad@gmail.com>
2017-02-17 23:32:35 +0700

    mengubah tampilan login

--- application/views/login.php
+++ application/views/login.php
@@ -10,53 +10,67 @@
   <link rel="stylesheet" type="text/css" href="<?= base_url().'mdb/' ?>css/style.css">
 </head>
 <body>
-  <div class="container-fluid">
+  <!--Navigation & Intro-->
+  <header>
 
-    <div class="card card-login">
-      <div class="card-block">
+      <!--Mask-->
+      <div class="view hm-black-strong">
+          <div class="full-bg-img flex-center">
+              <div class="container">
+                  <div class="row" id="home">
 
-        <!--Header-->
+                      <!--First column-->
+                      <div class="col-lg-6">
+                          <div class="description">
+                              <h2 class="h2-responsive wow fadeInLeft">Sign up right now! </h2>
+                              <hr class="hr-light wow fadeInLeft">
+                              <p class="wow fadeInLeft" data-wow-delay="0.4s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellendus quasi fuga nesciunt dolorum nulla magnam veniam sapiente, fugiat! Commodi sequi non animi ea dolor molestiae, quisquam iste, maiores. Nulla.</p>
+                              <br>
+                          </div>
+                      </div>
+                      <!--/.First column-->
 
-        <div class="form-header  yellow darken-1">
+                      <!--Second column-->
+                      <div class="col-lg-6 wow fadeInRight">
+                          <!--Form-->
+                          <div class="card">
+                              <div class="card-block">
+                                <!--Header-->
+                                <div class="form-header yellow darken-1">
+                                  <h3><i class="fa fa-user"></i> Login </h3>
+                                </div>
 
-          <h3><i class="fa fa-lock"></i> Login</h3>
-        </div>
+                                  <!--Body-->
+                                  <div class="md-form">
+                                      <i class="fa fa-envelope prefix"></i>
+                                      <input type="text" id="username" class="form-control">
+                                      <label for="username">Username</label>
+                                  </div>
 
-        <!--Body-->
-        <form action="Login/process_login" method="post">
+                                  <div class="md-form">
+                                      <i class="fa fa-lock prefix"></i>
+                                      <input type="text" id="password" class="form-control">
+                                      <label for="password">Password</label>
+                                  </div>
 
-          <div class="md-form">
-            <i class="fa fa-envelope prefix"></i>
-            <input type="text" id="form2" class="form-control" name="username">
-            <label for="form2">Username</label>
+                                  <div class="text-xs-center">
+                                      <button class="btn btn-primary btn-lg btn-block green">Sign up</button>
+
+                                  </div>
+
+                              </div>
+                          </div>
+                          <!--/.Form-->
+                      </div>
+                      <!--/Second column-->
+                  </div>
+              </div>
           </div>
-
-          <div class="md-form">
-            <i class="fa fa-lock prefix"></i>
-            <input type="password" id="form4" class="form-control" name="password">
-            <label for="form4">Password</label>
-          </div>
-
-          <div class="text-center">
-
-            <button class="btn btn-yellow" type="submit">Login</button>
-
-          </div>
-
-        </form>
-
       </div>
+      <!--/.Mask-->
 
-      <!--Footer-->
-      <div class="modal-footer">
-        <div class="options">
-          <p>Not a member? <a href="#">Sign Up</a></p>
-          <p>Forgot <a href="#">Password?</a></p>
-        </div>
-      </div>
-
-    </div>
-  </div>
+  </header>
+  <!--/Navigation & Intro-->
 
   <script type="text/javascript" src="<?= base_url().'mdb/' ?>js/jquery-3.1.1.min.js"></script>
   <script type="text/javascript" src="<?= base_url().'mdb/' ?>js/bootstrap.min.js"></script>
