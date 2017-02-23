9cf3cdb6d86ba0b0309cb01c5c775d2265e3a41b
nawawi932@gmail.com <nawawi932@gmail.com>
2017-02-23 13:32:47 +0700

    add new table

--- bower_components/db/eleven_guitar-2.sql
+++ bower_components/db/eleven_guitar-2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 21 Feb 2017 pada 09.15
+-- Generation Time: 23 Feb 2017 pada 07.31
 -- Versi Server: 10.1.19-MariaDB
 -- PHP Version: 7.0.9
 
@@ -70,25 +70,154 @@ INSERT INTO `barang` (`id_barang`, `nama_barang`, `id_kategori`, `foto`, `harga`
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kategori`
+-- Struktur dari tabel `barang_kategori`
 --
 
-CREATE TABLE `kategori` (
+CREATE TABLE `barang_kategori` (
   `id_kategori` int(10) NOT NULL,
   `nama_kategori` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kategori`
+-- Dumping data untuk tabel `barang_kategori`
 --
 
-INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+INSERT INTO `barang_kategori` (`id_kategori`, `nama_kategori`) VALUES
 (1, 'Gitar'),
 (2, 'Bass'),
 (3, 'Ampli'),
 (4, 'Effect'),
 (5, 'pickup'),
-(6, 'sdfdsf');
+(6, 'sdfdsf1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konten_pembayaran`
+--
+
+CREATE TABLE `konten_pembayaran` (
+  `id_kpembayaran` int(11) NOT NULL,
+  `nama_section` varchar(40) NOT NULL,
+  `judul_section` varchar(40) NOT NULL,
+  `isi_section` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konten_produk`
+--
+
+CREATE TABLE `konten_produk` (
+  `id_kproduk` int(11) NOT NULL,
+  `nama_section` varchar(40) NOT NULL,
+  `judul_section` varchar(40) NOT NULL,
+  `isi_section` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konten_testimoni`
+--
+
+CREATE TABLE `konten_testimoni` (
+  `id_ktestimoni` int(11) NOT NULL,
+  `judul_testimoni` varchar(30) NOT NULL,
+  `caption` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konten_utama`
+--
+
+CREATE TABLE `konten_utama` (
+  `id_kutama` int(11) NOT NULL,
+  `nama_section` varchar(30) NOT NULL,
+  `judul_section` varchar(40) NOT NULL,
+  `isi section` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `list_owner`
+--
+
+CREATE TABLE `list_owner` (
+  `id_listown` int(11) NOT NULL,
+  `nama_list` varchar(30) NOT NULL,
+  `caption` text NOT NULL,
+  `icon` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `list_pembayaran`
+--
+
+CREATE TABLE `list_pembayaran` (
+  `id_list` int(11) NOT NULL,
+  `nama_list` varchar(30) NOT NULL,
+  `icon` varchar(50) NOT NULL,
+  `caption` text NOT NULL,
+  `warna` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `list_testimoni`
+--
+
+CREATE TABLE `list_testimoni` (
+  `id_testimoni` int(11) NOT NULL,
+  `nama_user` varchar(30) NOT NULL,
+  `isi_testimoni` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `profil`
+--
+
+CREATE TABLE `profil` (
+  `id_profil` int(11) NOT NULL,
+  `gambar_header` varchar(100) NOT NULL,
+  `isi_profil` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `profil_owner`
+--
+
+CREATE TABLE `profil_owner` (
+  `id_powner` int(11) NOT NULL,
+  `nama_owner` varchar(30) NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `id_listown` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `slider`
+--
+
+CREATE TABLE `slider` (
+  `id_slider` int(11) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `logo` varchar(100) NOT NULL,
+  `judul` varchar(30) NOT NULL,
+  `caption` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -107,10 +236,70 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
--- Indexes for table `kategori`
+-- Indexes for table `barang_kategori`
 --
-ALTER TABLE `kategori`
+ALTER TABLE `barang_kategori`
   ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `konten_pembayaran`
+--
+ALTER TABLE `konten_pembayaran`
+  ADD PRIMARY KEY (`id_kpembayaran`);
+
+--
+-- Indexes for table `konten_produk`
+--
+ALTER TABLE `konten_produk`
+  ADD PRIMARY KEY (`id_kproduk`);
+
+--
+-- Indexes for table `konten_testimoni`
+--
+ALTER TABLE `konten_testimoni`
+  ADD PRIMARY KEY (`id_ktestimoni`);
+
+--
+-- Indexes for table `konten_utama`
+--
+ALTER TABLE `konten_utama`
+  ADD PRIMARY KEY (`id_kutama`);
+
+--
+-- Indexes for table `list_owner`
+--
+ALTER TABLE `list_owner`
+  ADD PRIMARY KEY (`id_listown`);
+
+--
+-- Indexes for table `list_pembayaran`
+--
+ALTER TABLE `list_pembayaran`
+  ADD PRIMARY KEY (`id_list`);
+
+--
+-- Indexes for table `list_testimoni`
+--
+ALTER TABLE `list_testimoni`
+  ADD PRIMARY KEY (`id_testimoni`);
+
+--
+-- Indexes for table `profil`
+--
+ALTER TABLE `profil`
+  ADD PRIMARY KEY (`id_profil`);
+
+--
+-- Indexes for table `profil_owner`
+--
+ALTER TABLE `profil_owner`
+  ADD PRIMARY KEY (`id_powner`);
+
+--
+-- Indexes for table `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`id_slider`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -127,10 +316,55 @@ ALTER TABLE `admin`
 ALTER TABLE `barang`
   MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
--- AUTO_INCREMENT for table `kategori`
+-- AUTO_INCREMENT for table `barang_kategori`
 --
-ALTER TABLE `kategori`
+ALTER TABLE `barang_kategori`
   MODIFY `id_kategori` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `konten_produk`
+--
+ALTER TABLE `konten_produk`
+  MODIFY `id_kproduk` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `konten_testimoni`
+--
+ALTER TABLE `konten_testimoni`
+  MODIFY `id_ktestimoni` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `konten_utama`
+--
+ALTER TABLE `konten_utama`
+  MODIFY `id_kutama` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `list_owner`
+--
+ALTER TABLE `list_owner`
+  MODIFY `id_listown` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `list_pembayaran`
+--
+ALTER TABLE `list_pembayaran`
+  MODIFY `id_list` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `list_testimoni`
+--
+ALTER TABLE `list_testimoni`
+  MODIFY `id_testimoni` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `profil`
+--
+ALTER TABLE `profil`
+  MODIFY `id_profil` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `profil_owner`
+--
+ALTER TABLE `profil_owner`
+  MODIFY `id_powner` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `id_slider` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
